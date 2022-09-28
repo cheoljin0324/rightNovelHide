1da8475f194b2b540700d585d7b43b12e77981c7
cheoljin0324 <77821929+cheoljin0324@users.noreply.github.com>
2022-09-28 20:27:13 +0900

    text add

--- rightnovelsearch/Assets/Scripts/Box.cs
+++ rightnovelsearch/Assets/Scripts/Box.cs
@@ -5,15 +5,23 @@ using UnityEngine.UI;
 
 public class Box : HideFloor
 {
+    //해당 Box안에 세팅 되어있는 아이템
     public GameObject settingItem;
+    //박스의 오브젝트
     GameObject inGameItem;
+    //아이템의 지정된 위치
     public Transform ItemTransform;
+    //박스의 state 상태
     FloorState boxState = FloorState.Idle;
+    //Player의 스크립트 컴포넌트
     PlayerScripts playerScripts;
+    //해당 박스의 이미지
     Image myimage;
 
+    //박스의 스프라이트(열림, 닫힘, 망가짐)
     public Sprite[] objectSprite;
 
+    //캐싱
     private void Awake()
     {
         playerScripts = FindObjectOfType<PlayerScripts>();
@@ -22,8 +30,11 @@ public class Box : HideFloor
 
     private void Start()
     {
+        //상태를 장애물로 변경
         boxState = FloorState.obstacle;
+        //아이템을 생성
         inGameItem = GameObject.Instantiate(settingItem);
+        //위치를 지정한 위치로 변경
         settingItem.transform.position = ItemTransform.transform.position;
 
 
@@ -31,22 +42,30 @@ public class Box : HideFloor
 
     public override void Obstacle()
     {
+        //박스 열었음
         Open();
+        //inGameItem의 오브젝트 enable을 false로 변경
         inGameItem.SetActive(false);
+        //만약 settingItem이 null 이 아닐 경우 플레이어 스크립트의 겟 아이템을 세팅된 아이템을 매개변수로 실행(준다 아이템을)
         if (settingItem != null) { playerScripts.GetItem(settingItem); }
+        //스프라이트를 열림 스프라이트로 변경
         myimage.sprite = objectSprite[0];
     }
 
+    //열림
     public override void Open()
+    {
+        //박스 스테이트를 close로 변경
+        boxState = FloorState.open;
+        //스프라이트 열림으로 변경
+        myimage.sprite = objectSprite[1];
+    }
+
+    //닫힘
+    public override void close()
     {
         boxState = FloorState.close;
         myimage.sprite = objectSprite[0];
-    }
-
-    public override void close()
-    {
-        boxState = FloorState.open;
-        myimage.sprite = objectSprite[1];
     }
 
 }
